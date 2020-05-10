a93d239369c6e29c365fa5e848aa72863404ac5d
Patricia Centenera Béjar <patriciacentenerabejar@gmail.com>
2020-05-10 13:09:47 +0200

    Express structure

--- index.js
+++ index.js
@@ -1,21 +1,10 @@
-let http = require('http');
-let fs = require('fs');
- 
-let handleRequest = (request, response) => {
-    response.writeHead(200, {
-        'Content-Type': 'text/html'
-    });
-    fs.readFile('./index.html', null, function (error, data) {
-        if (error) {
-            response.writeHead(404);
-            respone.write('Whoops! File not found!');
-        } else {
-            response.write(data);
-        }
-        response.end();
-    });
-};
- 
-const PORT = process.env.PORT || 3000;
+const path = require('path');
+const express = require('express');
 
-http.createServer(handleRequest).listen(PORT);
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.use('/', express.static(path.join(__dirname, 'public')));
+
+app.listen(port, () => console.log(`Listening on http://localhost:${port}/`));
