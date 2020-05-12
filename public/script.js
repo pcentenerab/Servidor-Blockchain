79350c59ba93b34abafc34707b68279df492275b
Patricia Centenera Béjar <patriciacentenerabejar@gmail.com>
2020-05-12 16:46:40 +0200

    First working version

--- public/script.js
+++ public/script.js
@@ -4,7 +4,7 @@ function setup() {
         drizzleState: {},
         account: ''
     }
-    
+
     // let drizzle know what contract we want
     const options = {
         contracts: [ Contador ],
